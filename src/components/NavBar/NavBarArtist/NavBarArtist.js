8c351df213a9ecedb9780cd9482a62a05f9befa7
Larry Cherry <larry.cherry.0406@gmail.com>
2019-03-13 05:33:21 -0500

    Updated Navbar and Artist container

---
+++ src/components/NavBar/NavBarArtist/NavBarArtist.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import {Link} from 'react-router-dom';
+import {MoreVert} from '@material-ui/icons';
+import {KeyboardBackspace} from '@material-ui/icons'
+import Button from '@material-ui/core/Button'
+import './NavBarArtist.css';
+
+const navBarNotifications = (props) => (
+    <div className='NavBarArtist'>
+    <AppBar position="static">
+        <Toolbar >
+            <Button className='NavBack'>
+            <Link to='/'>
+                <KeyboardBackspace/>
+            </Link>
+            </Button>
+            
+
+            <MoreVert/>
+        </Toolbar>
+    </AppBar>
+    </div>
+);
+
+export default navBarNotifications;
--- src/containers/Artist/Artist.js
+++ src/containers/Artist/Artist.js
@@ -1,21 +1,49 @@
 import React, {Component} from 'react'
+import adamImg from '../../assets/images/adamElara-mini.jpg';
+import './Artist.css';
+import NavBar from '../../components/NavBar/NavBar';
+import Button from '@material-ui/core/Button';
 
 class Artist extends Component {
 
     state = {
-        artist: {
-            id: 1,
-            name: 'Adam'
-        }
+        artist: [
+            {
+                id: 1,
+                name: 'Adam Elara',
+                image: adamImg
+            }
+
+        ]
     }
 
     render() {
-        console.log(this.props);
+        console.log(this.props.match.path);
         return (
-            <h1>
-                    some text {this.props.match.params.id}
-
-            </h1>
+            <React.Fragment>
+                <NavBar location={this.props.match.path}/>
+                <div className='Artist'>
+                    <div 
+                        className='artistImage'
+                        style={{backgroundImage: `url(${adamImg})`}}>
+                        <h1 style={{color: 'white'}}>{this.state.artist[0].name}</h1>
+                        <Button variant="contained">
+                            Follow
+                        </Button>
+                    </div>
+                    <div className='info'>
+                        <p>8.6b followers</p>
+                        <p>Checked into Houston 1d</p>
+                        <p>Followed by Chase13 +26 others</p>
+                        <p>Genre-fluid tunes, super solid dude. Vegan/Straight Edge.</p>
+                        <h2>Upcoming Show:</h2>
+                        <Button variant='contained'>
+                            <p>3/24 5:00pm</p>
+                            <p>Secret Group: Battle of the Bands!</p>
+                        </Button>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     } 
 }
